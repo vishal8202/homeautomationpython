d8728507e6a02f32cdd8b2096220c2bb4646f9bc
vishal8202 <vishalvijayan222@gmail.com>
2022-11-07 10:26:24 +0530

    first commit

---
+++ homeautomation.py
@@ -0,0 +1,16 @@
+while True:
+    print("""Select from the menu
+            1. add values
+            2. view all values
+            3. search with dates
+            4. exit \n""")
+    choice =int(input(f"enter your choice : "))
+    if choice==1:
+        print("Add values selected")
+    elif choice==2:
+        print("view all values selected")
+    elif choice==3:
+        print("search values with date selected")
+    
+    elif(choice==4):
+        break

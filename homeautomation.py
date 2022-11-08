f304a59e9b403a2c763ba67119faf3b760d98f98
vishal8202 <vishalvijayan222@gmail.com>
2022-11-08 11:57:14 +0530

    searchby date

--- homeautomation.py
+++ homeautomation.py
@@ -1,37 +1,50 @@
+from tabulate import tabulate
 import mysql.connector
 
 mydb = mysql.connector.connect(host = 'localhost', user = 'root', password='',database = 'homeautomationdb')
 
 mycursor = mydb.cursor()
+
 while True:
-    print("""Select from the menu
-            1. add values
-            2. view all values
-            3. search with dates
-            4. exit \n""")
-    choice =int(input(f"enter your choice : "))
-    if choice==1:
-        print("Add values selected")
+    print("\nSelect an option")
+    print("1. Add values")
+    print("2. View all values")
+    print("3. Search values using date")
+    print("4. Exit")
+
+    choice = int(input("Enter an option: "))
+    if(choice == 1):
+        print("Enter the values")
         temperature = input("Enther the temperature: ")
         humidity = input("Enter the humidity: ")
         moisture = input("Enter the moisture: ")
+
         sql = "INSERT INTO `sensorvalues`(`temperature`, `humidity`, `moisture`, `date`) VALUES (%s,%s,%s,now())"
         data = (temperature,humidity,moisture)
         mycursor.execute(sql,data)
         mydb.commit()
-        print("Data Inserted Successfully")
-    elif choice==2:
-        print("view all values selected")
+
+    elif(choice ==2):
+        print("View all values")
         sql = "SELECT `temperature`, `humidity`, `moisture`, `date` FROM `sensorvalues` "
         mycursor.execute(sql)
         result = mycursor.fetchall()
         for i in result:
             print(i)
-        
-    elif choice==3:
-        print("search values with date selected")
-        
 
-    
+
+    elif(choice == 3):
+        print("Search a value")
+      
+
+        date = input("enter the date : ")
+
+        sql = "SELECT `temperature`, `humidity`, `moisture`, `date` FROM sensorvalues WHERE `date`='"+date+"'"
+
+        mycursor.execute(sql)
+
+        result = mycursor.fetchall()
+
+        print(tabulate(result,headers=['temperature','humidity','moisture','date'],tablefmt="psql"))
     elif(choice==4):
         break
